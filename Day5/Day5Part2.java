d8f3f776719ed8cf61c5036359db488574df4edd
Arun Donti <dontirun@gmail.com>
2017-12-05 18:20:11 -0500

    Day 5 solutions

--- Day5/Day5Part2.java
+++ Day5/Day5Part2.java
@@ -3,10 +3,9 @@ package Day5;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
-public class Day5Part1 {
+public class Day5Part2 {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner scan = new Scanner(new File("JumpOffsets"));
         ArrayList<Integer> jumps = new ArrayList<>();
@@ -22,7 +21,13 @@ public class Day5Part1 {
         while(index < jumps.size()){
             prevIndex= index;
             index += jumps.get(index);
-            jumps.set(prevIndex, jumps.get(prevIndex)+1);
+            boolean increase = jumps.get(prevIndex)<3;
+            if(increase){
+                jumps.set(prevIndex, jumps.get(prevIndex)+1);
+            }
+            else{
+                jumps.set(prevIndex, jumps.get(prevIndex)-1);
+            }
             totalSteps++;
         }
 
